ecdd79e6e9a01d3963a5470f39fecfd2eff07ac3
Dimcho Lakov <mdsektor@gmail.com>
2021-10-18 16:47:54 +0100

    Add Microsoft.Extensions.DependencyInjection package and DomainConfiguration

---
+++ MyOnlineShop/MyOnlineShop.Domain/DomainConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace MyOnlineShop.Domain
+{
+    using Microsoft.Extensions.DependencyInjection;
+    
+    using MyOnlineShop.Domain.Common;
+
+    public static class DomainConfiguration
+    {
+        public static IServiceCollection AddFactories(this IServiceCollection services)
+        {
+            services
+                .Scan(scan => scan.FromCallingAssembly()
+                .AddClasses(classes => classes
+                    .AssignableTo(typeof(IFactory<>)))
+                .AsMatchingInterface()
+                .WithTransientLifetime());
+
+            return services;
+        }
+    }
+}
