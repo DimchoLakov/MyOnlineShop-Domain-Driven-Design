c36c75be0ab5bc20778c5ce09b5dc5337fb34c4f
Dimcho Lakov <mdsektor@gmail.com>
2021-10-18 16:47:27 +0100

    Add IProductDomainRepository

---
+++ MyOnlineShop/MyOnlineShop.Domain/Catalog/Repositories/Products/IProductDomainRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace MyOnlineShop.Domain.Catalog.Repositories.Products
+{
+    using MyOnlineShop.Domain.Catalog.Models.Products;
+    using MyOnlineShop.Domain.Common;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IProductDomainRepository : IDomainRepository<Product>
+    {
+        Task<Product> Find(int id, CancellationToken cancellationToken = default);
+
+        Task<bool> Delete(int id, CancellationToken cancellationToken = default);
+
+        Task<ProductOption> GetProductOption(int productId, string optionName, CancellationToken cancellationToken = default);
+
+        Task<ProductOption> GetProductOption(int productId, int optionId, CancellationToken cancellationToken = default);
+
+        Task<bool> DeleteProductOption(int productId, int optionId, CancellationToken cancellationToken = default);
+    }
+}
