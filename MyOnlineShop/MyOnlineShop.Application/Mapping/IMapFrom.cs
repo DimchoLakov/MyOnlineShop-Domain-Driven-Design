7e57256a1e869ad9b389f784a0b1c852ca43bf35
Dimcho Lakov <mdsektor@gmail.com>
2021-10-18 18:40:57 +0100

    Add Application Layer. Add Automapper, IMapFrom interface and MappingProfile with automatic mapping

---
+++ MyOnlineShop/MyOnlineShop.Application/Mapping/IMapFrom.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace MyOnlineShop.Application.Mapping
+{
+    public interface IMapFrom<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), this.GetType());
+    }
+}
