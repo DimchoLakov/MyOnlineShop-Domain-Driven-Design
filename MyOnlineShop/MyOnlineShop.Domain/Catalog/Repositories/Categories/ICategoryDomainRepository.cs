5f6ea7c20fb656bb8840c365e53b31ef6657aa14
Dimcho Lakov <mdsektor@gmail.com>
2021-10-18 17:54:37 +0100

    Add ICategoryDomainRepository

---
+++ MyOnlineShop/MyOnlineShop.Domain/Catalog/Repositories/Categories/ICategoryDomainRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace MyOnlineShop.Domain.Catalog.Repositories.Categories
+{
+    using MyOnlineShop.Domain.Catalog.Models.Categories;
+    using MyOnlineShop.Domain.Common;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface ICategoryDomainRepository : IDomainRepository<Category>
+    {
+        Task<Category> Find(int id, CancellationToken cancellationToken = default);
+
+        Task<Category> Find(string name, CancellationToken cancellationToken = default);
+
+        Task<bool> Delete(int id, CancellationToken cancellationToken = default);
+    }
+}
