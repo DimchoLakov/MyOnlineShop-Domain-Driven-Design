cd5af5574e1e382efa3ca4fda149d09598dba484
Dimcho Lakov <mdsektor@gmail.com>
2021-10-19 08:52:02 +0100

    Add ApplicationConfiguration

---
+++ MyOnlineShop/MyOnlineShop.Application/ApplicationConfiguration.cs
@@ -0,0 +1,39 @@
+﻿namespace MyOnlineShop.Application
+{
+    using MediatR;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using MyOnlineShop.Application.Common;
+    using MyOnlineShop.Application.Common.Behaviours;
+    using System.Reflection;
+
+    public static class ApplicationConfiguration
+    {
+        public static IServiceCollection AddApplicationConfiguration(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services
+                .Configure<ApplicationSettings>(
+                    configuration.GetSection(nameof(ApplicationSettings)),
+                    builder => builder.BindNonPublicProperties = true)
+                .AddAutoMapper(Assembly.GetExecutingAssembly())
+                .AddMediatR(Assembly.GetExecutingAssembly())
+                .AddEventHandlers()
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestBehaviourPipeline<,>));
+
+            return services;
+        }
+
+        private static IServiceCollection AddEventHandlers(this IServiceCollection services)
+        {
+            services
+                .Scan(scan => scan.FromCallingAssembly()
+                                  .AddClasses(classes => classes.AssignableTo(typeof(IEventHandler<>)))
+                                  .AsImplementedInterfaces()
+                                  .WithTransientLifetime());
+
+            return services;
+        }
+    }
+}
