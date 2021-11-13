7d6898146f61e8bdd34ec65bac37416f78dd77bb
Dimcho Lakov <mdsektor@gmail.com>
2021-11-13 04:03:04 +0000

    Add ClearShoppingCartCommandValidator

---
+++ MyOnlineShop/MyOnlineShop.Application/Shopping/Commands/ClearCart/ClearShoppingCartCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace MyOnlineShop.Application.Shopping.Commands.ClearCart
+{
+    using FluentValidation;
+
+    public class ClearShoppingCartCommandValidator : AbstractValidator<ClearShoppingCartCommand>
+    {
+        public ClearShoppingCartCommandValidator()
+        {
+            this.RuleFor(c => c.UserId)
+                .NotEmpty();
+        }
+    }
+}
