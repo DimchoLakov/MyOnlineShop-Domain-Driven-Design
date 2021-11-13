47f39893dcf0ed2d4d625a61c91329e681398fc4
Dimcho Lakov <mdsektor@gmail.com>
2021-11-13 04:01:58 +0000

    Add RemoveCartItemCommandValidator

---
+++ MyOnlineShop/MyOnlineShop.Application/Shopping/Commands/RemoveCartItem/RemoveCartItemCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace MyOnlineShop.Application.Shopping.Commands.RemoveCartItem
+{
+    using FluentValidation;
+
+    public class RemoveCartItemCommandValidator : AbstractValidator<RemoveCartItemCommand>
+    {
+        public RemoveCartItemCommandValidator()
+        {
+            this.RuleFor(r => r.ProductId)
+                .Must(p => p > 0);
+
+            this.RuleFor(r => r.UserId)
+                .NotEmpty();
+        }
+    }
+}
