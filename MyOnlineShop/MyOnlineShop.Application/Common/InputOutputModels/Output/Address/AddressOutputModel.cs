941f6e779b786c1333db69b859f43a3c5184a2bf
Dimcho Lakov <mdsektor@gmail.com>
2021-11-16 11:45:31 +0000

    Add Address to Order Details

---
+++ MyOnlineShop/MyOnlineShop.Application/Common/InputOutputModels/Output/Address/AddressOutputModel.cs
@@ -0,0 +1,15 @@
+﻿using MyOnlineShop.Application.Common.Mapping;
+
+namespace MyOnlineShop.Application.Common.InputOutputModels.Output.Address
+{
+    public class AddressOutputModel : IMapFrom<Domain.Ordering.Models.Orders.Address>
+    {
+        public string AddressLine { get; set; } = default!;
+
+        public string PostCode { get; set; } = default!;
+
+        public string Town { get; set; } = default!;
+
+        public string Country { get; set; } = default!;
+    }
+}
--- MyOnlineShop/MyOnlineShop.Application/Ordering/Queries/Details/Models/OrderDetailsOutputModel.cs
+++ MyOnlineShop/MyOnlineShop.Application/Ordering/Queries/Details/Models/OrderDetailsOutputModel.cs
@@ -1,12 +1,15 @@
 ﻿namespace MyOnlineShop.Application.Ordering.Queries.Details.Models
 {
     using AutoMapper;
+    using MyOnlineShop.Application.Common.InputOutputModels.Output.Address;
     using MyOnlineShop.Application.Ordering.Queries.Common;
     using MyOnlineShop.Domain.Ordering.Models.Orders;
     using System.Collections.Generic;
 
     public class OrderDetailsOutputModel : OrderOutputModel
     {
+        public AddressOutputModel Address { get; set; } = default!;
+
         public IEnumerable<OrderItemDetailsOutputModel> OrderItems { get; set; } = new List<OrderItemDetailsOutputModel>();
 
         public override void Mapping(Profile profile)
